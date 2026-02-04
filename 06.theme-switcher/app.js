71080fb9d6be2bd49ca8924b9ecb040620f3086a
codezenashish <ixtashish@gmail.com>
2026-02-04 12:56:30 +0530

    add theme persistence with localStorage and initial theme setup

--- 06.theme-switcher/app.js
+++ 06.theme-switcher/app.js
@@ -1,12 +1,20 @@
 const themeHendler = document.querySelector("#theme-switcher");
+const body = document.querySelector("body");
 
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+  body.classList.add("dark");
+  themeHendler.textContent = "Light Mode";
+}
 themeHendler.addEventListener("click", () => {
-  let body = document.querySelector("body");
+  let theme = "light";
   body.classList.toggle("dark");
-
   if (body.classList.contains("dark")) {
+    theme = "dark";
     themeHendler.textContent = "Light Mode";
   } else {
     themeHendler.textContent = "Dark Mode";
   }
+  localStorage.setItem("theme", theme);
 });
